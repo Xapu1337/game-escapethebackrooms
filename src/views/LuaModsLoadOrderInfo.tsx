6306e459d0305c85d39ea663abe641a0842d41cc
Xapu1337 <xapursplaz@gmail.com>
2025-07-23 17:28:09 +0200

    change game art, change text for ue4ss and rename author (while crediting the original)

--- src/views/LuaModsLoadOrderInfo.tsx
+++ src/views/LuaModsLoadOrderInfo.tsx
@@ -5,7 +5,7 @@ function LuaModsLoadOrderInfo() {
         <div style={{paddingLeft: '8px'}}>
             <h3>Lua Mods</h3>
             <p>This page allows you to enabled and disable Lua script mods which have been installed to the EscapeTheBackrooms\Binaries\Win64\Mods folder.</p>
-            <p>Lua mods are used to inject changes into the game without requiring a PAK file. <a href='https://www.nexusmods.com/escapethebackrooms/mods/942'>UE4SS</a> must be installed for these mods to work correctly.</p>
+            <p>Lua mods are used to inject changes into the game without requiring a PAK file. <a href='https://github.com/UE4SS-RE/RE-UE4SS/releases/tag/v3.0.1'>UE4SS</a> must be installed for these mods to work correctly.</p>
         </div>
     );
 }
